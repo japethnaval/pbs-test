e5b270f486075e2ec51cd68792e3ee5c057ca810
japethnaval <jhapnavs@gmail.com>
2025-02-05 23:39:33 +0800

    remove express

--- express-app/src/index.ts
+++
@@ -1,8 +0,0 @@
-import { initServer } from "./init-app";
-
-const app = initServer();
-const PORT = 3232;
-
-app.listen(PORT, () => {
-	console.log(`Running on Port ${PORT}`);
-});
--- express-app/src/routes/referrals.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { addReferral, getReferrals } from "../handlers/referrals";
-
-const router = Router();
-
-router.get("/", getReferrals);
-router.post("/", addReferral);
-
-export default router;
